00e3ef83e50102fd55a1b4aa4c24830594595c48
¨ester¨ <estercika83@gmail.com>
2025-01-10 06:49:03 -0500

    ¨hi¨

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -16,11 +16,18 @@ public class Problema1 {
      */
     public static void main(String[] args) {
         int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-        imprimirDatos(informacion);
+        imprimirPares(informacion);
+        };
+    
+    public static void imprimirPares(int[][] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) { // Recorrer filas
+            for (int j = 0; j < arreglo[i].length; j++) { // Recorrer columnas
+                if (arreglo[i][j] % 2 == 0) { // Verificar si el valor es par
+                    System.out.println("Valor par encontrado: " + arreglo[i][j]);
+                }
+            }
+        }
     }
-
-    public static void imprimirDatos(int[][] datos) {
-
-    }
-
 }
+
+
